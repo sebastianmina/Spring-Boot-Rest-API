cd2f6d976fef64912726b6983995bb93ddff9891
sebastianmina <juan.mina@mail.escuelaing.edu.co>
2022-02-02 09:57:26 -0500

    DTO

---
+++ src/main/java/co/escuelaing/User/Dto/UserDTO.java
@@ -0,0 +1,9 @@
+package co.escuelaing.User.Dto;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private String name;
+    private String email;
+    private String lastname;
+}
